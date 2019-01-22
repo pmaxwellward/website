6c77699674a81b3503d6b037d437cac798d70387
pmaxwellward <pmaxwellward@gmail.com>
2019-01-22 17:17:53 -0600

    updated thumbnail slider

--- js/work.js
+++ js/work.js
@@ -43,16 +43,15 @@ for (i = 0; i < 18; i++) {
 for (i = 0; i < illoSlides.length; i++) {
   var picture = document.createElement('img');
   picture.src = `./images/illo_`+i+`_t.png`;
-  picture.className = "demo";
-  picture.classList.add("column");
+  picture.className = "thumb-status";
   thumbs[i].appendChild(picture);
 }
 
 var illoThumbs;
 const pic = document.querySelectorAll('.thumbnails img');
-setTimeout(function() {
+//setTimeout(function() {
   illoThumbs = Array.from(pic);
-}, 100);
+//}, 100);
 var i;
 pic.forEach(e => e.addEventListener('click', function() {
   i = illoThumbs.indexOf(e);
@@ -69,22 +68,26 @@ function closeModal() {
 }
 
 var slideIndex = 0;
-showSlides(slideIndex);
 
 //next/forward controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 //thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
+  for(x=0;x<illoThumbs.length;x++) {
+    illoThumbs[x].setAttribute("class","thumb-status");
+  }
   var captionText = document.getElementById("caption");
   if (n >= illoSlides.length) {slideIndex = 0}
   if (n < 0) {slideIndex = illoSlides.length-1}
   slides.firstElementChild.src = illoSlides[slideIndex];
   captionText.innerHTML = captionList[slideIndex];
+  illoThumbs[slideIndex].setAttribute("class","active");
+  document.getElementById("thumbnail-slider").scrollTo((pic[slideIndex].offsetLeft-234),0);
 }
