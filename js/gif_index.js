e815731fd6bfc407ec25f289303a5bc48fc4a124
pmaxwellward <pmaxwellward@gmail.com>
2019-02-26 16:48:43 -0600

    GIF objects

--- js/gif_index.js
+++ js/gif_index.js
@@ -1,3 +1,15 @@
-var gifMan = document.getElementById("gif-Man");
+const gifMan = document.querySelector(".gif-Man img");
+const man_00 = {name: "GIFs/00.gif", length: 1041.666};
+const man_01 = {name: "GIFs/01.gif", length: 1375};
+const man_02 = {name: "GIFs/02.gif", length: 875};
+const man_03 = {name: "GIFs/03.gif", length: 1750};
+const man_04 = {name: "GIFs/04.gif", length: 458.333};
+const man_05 = {name: "GIFs/05.gif", length: 250};
+const man_06 = {name: "GIFs/06.gif", length: 416.666};
+const man_07 = {name: "GIFs/07.gif", length: 250};
+const man_08 = {name: "GIFs/08.gif", length: 500};
+const man_09 = {name: "GIFs/09.gif", length: 625};
 
-setTimeout(function() {gifMan.firstChildElement.src = "GIFs/01.gif";}, 1041.666);
+
+    setTimeout(function() {
+        gifMan.src = man_01.name;}, man_00.length);
