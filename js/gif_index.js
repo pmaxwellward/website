39e94cac5b13f7fcdbd712be0cfd5500285e1d62
pmaxwellward <pmaxwellward@gmail.com>
2019-03-03 23:12:34 -0600

    textBox GIFs added

--- js/gif_index.js
+++ js/gif_index.js
@@ -1,26 +1,209 @@
+var manGIFs = [];
+var angerGIFs = [];
+var blurbGIFs = [];
+var firstGIFs = [];
+var introGIFs = [];
+
+function prefetch() {
+    for (var i = 0; i < arguments.length; i++) {
+        manGIFs[i] = new Image();
+        manGIFs[i].src = prefetch.arguments[i];
+    }
+}
+
+prefetch(
+    "GIFs/00.gif",
+    "GIFs/01.gif",
+    "GIFs/02.gif",
+    "GIFs/03.gif",
+    "GIFs/04.gif",
+    "GIFs/05.gif",
+    "GIFs/06.gif",
+    "GIFs/07.gif",
+    "GIFs/08.gif",
+    "GIFs/09.gif",
+    "GIFs/10.gif",
+    "GIFs/11.gif",
+    "GIFs/12.gif",
+    "GIFs/13.gif",
+    "GIFs/14.gif",
+    "GIFs/15.gif",
+    "GIFs/16.gif",
+    "GIFs/17.gif",
+    "GIFs/18.gif",
+    "GIFs/19.gif",
+    "GIFs/20.gif",
+    "GIFs/21.gif"
+)
+
+function pushAnger() {
+    for (var i = 0; i < arguments.length; i++) {
+        angerGIFs[i] = new Image();
+        angerGIFs[i].src = pushAnger.arguments[i];
+    }
+}
+function pushBlurb() {
+    for (var i = 0; i < arguments.length; i++) {
+        blurbGIFs[i] = new Image();
+        blurbGIFs[i].src = pushBlurb.arguments[i];
+    }
+}
+function pushFirst() {
+    for (var i = 0; i < arguments.length; i++) {
+        firstGIFs[i] = new Image();
+        firstGIFs[i].src = pushFirst.arguments[i];
+    }
+}
+function pushIntro() {
+    for (var i = 0; i < arguments.length; i++) {
+        introGIFs[i] = new Image();
+        introGIFs[i].src = pushIntro.arguments[i];
+    }
+}
+pushAnger(
+    "GIFs/anger_00.gif",
+    "GIFs/anger_01.gif",
+    "GIFs/anger_02.gif",
+    "GIFs/anger_03.gif",
+    "GIFs/anger_04.gif",
+    "GIFs/anger_05.gif",
+    "GIFs/anger_06.gif",
+    "GIFs/anger_07.gif",
+    "GIFs/anger_08.gif",
+    "GIFs/anger_09a.gif",
+    "GIFs/anger_09b.gif",
+    "GIFs/anger_10.gif",
+    "GIFs/anger_11.gif",
+    "GIFs/anger_12.gif",
+    "GIFs/anger_13.gif",
+    "GIFs/anger_14.gif",
+    "GIFs/anger_15.gif"
+)
+pushBlurb(
+    "GIFs/blurb_00a.gif",
+    "GIFs/blurb_00b.gif",
+    "GIFs/blurb_01a.gif",
+    "GIFs/blurb_01b.gif",
+    "GIFs/blurb_02a.gif",
+    "GIFs/blurb_02b.gif",
+    "GIFs/blurb_03a.gif",
+    "GIFs/blurb_03b.gif",
+    "GIFs/blurb_04.gif",
+    "GIFs/blurb_05a.gif",
+    "GIFs/blurb_05b.gif",
+    "GIFs/blurb_05c.gif",
+    "GIFs/blurb_06a.gif",
+    "GIFs/blurb_06b.gif",
+    "GIFs/blurb_07.gif",
+    "GIFs/blurb_08a.gif",
+    "GIFs/blurb_08b.gif",
+    "GIFs/blurb_09.gif",
+    "GIFs/blurb_10a.gif",
+    "GIFs/blurb_10b.gif",
+    "GIFs/blurb_11.gif",
+    "GIFs/blurb_12a.gif",
+    "GIFs/blurb_12b.gif",
+    "GIFs/blurb_13a.gif",
+    "GIFs/blurb_13b.gif",
+    "GIFs/blurb_14a.gif",
+    "GIFs/blurb_14b.gif",
+    "GIFs/blurb_15.gif",
+    "GIFs/blurb_16a.gif",
+    "GIFs/blurb_16b.gif",
+    "GIFs/blurb_17a.gif",
+    "GIFs/blurb_17b.gif",
+    "GIFs/blurb_17c.gif"
+)
+pushFirst(
+    "GIFs/first_00.gif",
+    "GIFs/first_01.gif",
+    "GIFs/first_02.gif",
+    "GIFs/first_03.gif"
+)
+pushIntro(
+    "GIFs/intro_00a.gif",
+    "GIFs/intro_00b.gif",
+    "GIFs/intro_01a.gif",
+    "GIFs/intro_01b.gif",
+    "GIFs/intro_02.gif",
+    "GIFs/intro_03a.gif",
+    "GIFs/intro_03b.gif",
+    "GIFs/intro_04a.gif",
+    "GIFs/intro_04b.gif"  
+)
+
+const man_00 = {name: manGIFs[0].src, length: 625};
+const man_01 = {name: manGIFs[1].src, length: 1375};
+const man_02 = {name: manGIFs[2].src, length: 875};
+const man_03 = {name: manGIFs[3].src, length: 1750};
+const man_04 = {name: manGIFs[4].src, length: 458.333};
+const man_05 = {name: manGIFs[5].src, length: 250};
+const man_06 = {name: manGIFs[6].src, length: 416.666};
+const man_07 = {name: manGIFs[7].src, length: 250};
+const man_08 = {name: manGIFs[8].src, length: 500};
+const man_09 = {name: manGIFs[9].src, length: 625};
+const fly_10 = {name: manGIFs[10].src, length: 1333.333};
+const fly_11 = {name: manGIFs[11].src, length: 1166.666};
+const fly_12 = {name: manGIFs[12].src, length: 1125};
+const fly_13 = {name: manGIFs[13].src, length: 1333.333};
+const fly_14 = {name: manGIFs[14].src, length: 1166.666};
+const fly_15 = {name: manGIFs[15].src, length: 1125};
+const man_16 = {name: manGIFs[16].src, length: 1666.666};
+const man_17 = {name: manGIFs[17].src, length: 1166.666};
+const man_18 = {name: manGIFs[18].src, length: 166.666};
+const man_19 = {name: manGIFs[19].src, length: 833.333};
+const man_20 = {name: manGIFs[20].src, length: 1875};
+const man_21 = {name: manGIFs[21].src, length: 333.333};
+
+const anger_00 = angerGIFs[0].src;
+const anger_01 = angerGIFs[1].src;
+const anger_02 = angerGIFs[2].src;
+const anger_03 = angerGIFs[3].src;
+const anger_04 = angerGIFs[4].src;
+const anger_05 = angerGIFs[5].src;
+const anger_06 = angerGIFs[6].src;
+const anger_07 = angerGIFs[7].src;
+const anger_08 = angerGIFs[8].src;
+const anger_09 = [angerGIFs[9].src, angerGIFs[10].src];
+const anger_10 = angerGIFs[11].src;
+const anger_11 = angerGIFs[12].src;
+const anger_12 = angerGIFs[13].src;
+const anger_13 = angerGIFs[14].src;
+const anger_14 = angerGIFs[15].src;
+const anger_15 = angerGIFs[16].src;
+
+const blurb_00 = [blurbGIFs[0].src,blurbGIFs[1].src];
+const blurb_01 = [blurbGIFs[2].src,blurbGIFs[3].src]
+const blurb_02 = [blurbGIFs[4].src,blurbGIFs[5].src]; 
+const blurb_03 = [blurbGIFs[6].src,blurbGIFs[7].src];
+const blurb_04 = blurbGIFs[8].src;
+const blurb_05 = [blurbGIFs[9].src,blurbGIFs[10].src,blurbGIFs[11].src];
+const blurb_06 = [blurbGIFs[12].src,blurbGIFs[13].src];
+const blurb_07 = blurbGIFs[14].src;
+const blurb_08 = [blurbGIFs[15].src,blurbGIFs[16].src];
+const blurb_09 = blurbGIFs[17].src;
+const blurb_10 = [blurbGIFs[18].src,blurbGIFs[19].src];
+const blurb_11 = blurbGIFs[20].src;
+const blurb_12 = [blurbGIFs[21].src,blurbGIFs[22].src];
+const blurb_13 = [blurbGIFs[23].src,blurbGIFs[24].src];
+const blurb_14 = [blurbGIFs[25].src,blurbGIFs[26].src];
+const blurb_15 = blurbGIFs[27].src;
+const blurb_16 = [blurbGIFs[28].src,blurbGIFs[29].src];
+const blurb_17 = [blurbGIFs[30].src,blurbGIFs[31].src,blurbGIFs[32].src];
+
+const first_00 = firstGIFs[0].src;
+const first_01 = firstGIFs[1].src;
+const first_02 = firstGIFs[2].src;
+const first_03 = firstGIFs[3].src;
+
+const intro_00 = [introGIFs[0].src,introGIFs[1].src];
+const intro_01 = [introGIFs[2].src,introGIFs[3].src];
+const intro_02 = introGIFs[4].src;
+const intro_03 = [introGIFs[5].src,introGIFs[6].src];
+const intro_04 = [introGIFs[7].src,introGIFs[8].src];
+
 const gifMan = document.querySelector(".gif-Man img");
-const man_00 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/00.gif", length: 625};
-const man_01 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/01.gif", length: 1375};
-const man_02 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/02.gif", length: 875};
-const man_03 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/03.gif", length: 1750};
-const man_04 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/04.gif", length: 458.333};
-const man_05 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/05.gif", length: 250};
-const man_06 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/06.gif", length: 416.666};
-const man_07 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/07.gif", length: 250};
-const man_08 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/08.gif", length: 500};
-const man_09 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/09.gif", length: 625};
-const fly_10 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/10.gif", length: 1333.333};
-const fly_11 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/11.gif", length: 1166.666};
-const fly_12 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/12.gif", length: 1125};
-const fly_13 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/13.gif", length: 1333.333};
-const fly_14 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/14.gif", length: 1166.666};
-const fly_15 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/15.gif", length: 1125};
-const man_16 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/16.gif", length: 1666.666};
-const man_17 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/17.gif", length: 1166.666};
-const man_18 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/18.gif", length: 166.666};
-const man_19 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/19.gif", length: 833.333};
-const man_20 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/20.gif", length: 1875};
-const man_21 = {name: "file:///C:/Users/Graphics/Documents/website/GIFs/21.gif", length: 333.333};
+const textBox = document.querySelector(".text-Box img");
 
 var idleLoop;
 var idleCycle;
@@ -30,9 +213,70 @@ var flyIndex = 0;
 var buttonOn = false;
 var firstPress = false;
 var range = [3,4,5];
-var fly = [fly_10,fly_11,fly_12,fly_13,fly_14,fly_15];
 
+var fly = [
+    fly_10,
+    fly_11,
+    fly_12,
+    fly_13,
+    fly_14,
+    fly_15];
 
+var anger = [
+    anger_00,
+    anger_01,
+    anger_02,
+    anger_03,
+    anger_04,
+    anger_05,
+    anger_06,
+    anger_07,
+    anger_08,
+    anger_09,
+    anger_10,
+    anger_11,
+    anger_12,
+    anger_13,
+    anger_14,
+    anger_15];
+
+var blurb = [   
+    blurb_00,
+    blurb_01,
+    blurb_02,
+    blurb_03,
+    blurb_04,
+    blurb_05,
+    blurb_06,
+    blurb_07,
+    blurb_08,
+    blurb_09,
+    blurb_10,
+    blurb_11,
+    blurb_12,
+    blurb_13,
+    blurb_14,
+    blurb_15,
+    blurb_16,
+    blurb_17];
+
+var first = [   
+    first_00,
+    first_01,
+    first_02,
+    first_03];
+
+var intro = [
+    intro_00,
+    intro_01,
+    intro_02,
+    intro_03,
+    intro_04];
+
+shuffle(anger);
+shuffle(blurb);
+shuffle(first);
+shuffle(intro);
 
 function playScene() {
     switch(gifMan.src) {
